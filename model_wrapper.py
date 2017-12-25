617fde1f2a43d3fe2decfbbeda7136119c7f605d
txdat <altairdtx@gmail.com>
2017-12-26 03:41:34 +0700

    Update model_wrapper.py

--- model_wrapper.py
+++ model_wrapper.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 
 def get_model(name):
     if name == 'vgg_16':
-        from nets.mobilenet import model, forward
+        from nets.vgg import model, forward
     if name == 'MobilenetV1':
         from nets.mobilenet import model, forward
     else:
